cf3a4e3ea67feacfa0e462e0cccdf76faa178db6
bala-amypo <bala@amypo.in>
2025-12-21 17:18:06 +0000

    User commit

--- src/main/java/com/example/demo/service/ActivityLogService.java
+++ src/main/java/com/example/demo/service/ActivityLogService.java
@@ -1,16 +1,9 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.ActivityLog;
-import java.time.LocalDate;
 import java.util.List;
 
 public interface ActivityLogService {
-
-    ActivityLog logActivity(Long userId, Long typeId, ActivityLog log);
-
-    ActivityLog getLog(Long id);
-
-    List<ActivityLog> getLogsByUser(Long userId);
-
-    List<ActivityLog> getLogsByUserAndDate(Long userId, LocalDate start, LocalDate end);
+    ActivityLog createActivityLog(ActivityLog log);
+    List<ActivityLog> getAllLogs();
 }
